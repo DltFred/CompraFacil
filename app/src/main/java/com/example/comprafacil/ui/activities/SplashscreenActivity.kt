53e96c9464023d1a8296a5b6328a2dbc9c7f5ce9
FredDlt <fredy.delatorre.frd@gmail.com>
2020-11-09 01:00:09 -0500

    primer commit

---
+++ app/src/main/java/com/example/comprafacil/ui/activities/SplashscreenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.comprafacil.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.comprafacil.R
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
